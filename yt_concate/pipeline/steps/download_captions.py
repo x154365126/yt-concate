23328a8c3c8fd4b0826038583960a5dae4dfc5e6
ycc <x154365126@gmail.com>
2022-07-19 23:40:20 +0800

    Finished downloading videos

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,36 +1,37 @@
 import os
+import time
 
 from pytube import YouTube
 
 from .step import Step
 from .step import StepException
 
-import time
-
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
-        for url in data:
-            print('downloading caption for', url)
-            if utils.caption_file_exists(url):
+        for yt in data: # url
+            print('downloading caption for', yt.id) # url
+            if utils.caption_file_exists(yt):
                 print('found existing cation file')
                 continue
 
             try:
-                source = YouTube(url)
+                source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
                 en_caption_convert_to_srt = (en_caption.generate_srt_captions())
                 # print(en_caption_convert_to_srt)
 
             except (KeyError, AttributeError):
-                print('Error when downloading caption for', url)
+                print('Error when downloading caption for', yt.url)
                 continue
 
             # save the caption to a file named Output.txt
-            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
+            text_file = open(utils.caption_filepath(url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
 
         end = time.time()
         print('took', end - start, 'seconds')
+
+        return data
---
+++ yt_concate/pipeline/steps/download_videos.py
@@ -0,0 +1,24 @@
+from .step import Step
+
+from pytube import YouTube
+from yt_concate.settings import VIDEOS_DIR
+
+
+class DownloadVideos(Step):
+    def process(self, data, inputs, utils):
+        # print(len(data))
+        yt_set = set([found.yt for found in data])
+        # print(len(yt_set))
+        print('videos to download=', len(yt_set))
+        for yt in yt_set:
+            # yt = found.yt
+            url = yt.url
+
+            if utils.video_file_exists(yt):
+                print(f'found existing video file for {url}, skipping')
+                continue
+
+            print('downloading', url)
+            YouTube(url).streams.get_by_resolution('360p').download(output_path=VIDEOS_DIR, filename=yt.id + '.mp4')
+
+        return data
